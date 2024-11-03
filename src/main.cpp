bb6da0ccb2415c6168e15ad15d8cdb8048965ceb
Nicolas Papirakis <nicolaspapirakis@Nicolass-Laptop.local>
2024-11-02 17:09:13 -0700

    added imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,120 @@
+#include <glad/glad.h>
+#include <SDL.h>
 #include <iostream>
 
-int main(int argc, char **argv)
-{
-    std::cout << "Hello" << std::endl;
+// imgui
+#include "imgui.h"
+#define IMGUI_IMPL_OPENGL_LOADER_GLAD
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_sdl2.h"
+
+// constants
+constexpr int WIDTH = 1280;
+constexpr int HEIGHT = 720;
+constexpr const char *TITLE = "Minecrap";
+
+int main(int argc, char **argv) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "Failed to init SDL" << std::endl;
+        return -1;
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+#ifdef __APPLE__
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+#endif
+
+    SDL_Window *window =
+        SDL_CreateWindow(TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                         WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+
+    if (window == nullptr) {
+        std::cerr << "Failed to open window" << std::endl;
+        return -1;
+    }
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    if (context == nullptr) {
+        std::cerr << "Failed to create OpenGL context" << std::endl;
+        return -1;
+    }
+
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+        std::cerr << "Failed to load OpenGL extensions" << std::endl;
+        return -1;
+    }
+
+    SDL_GL_MakeCurrent(window, context);
+
+    glViewport(0.0f, 0.0f, WIDTH, HEIGHT);
+    glClearColor(0.0f, 0.65f, 1.0f, 1.0f);
+
+    // Initialize ImGui
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGuiIO &io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+
+	ImGuiStyle &style = ImGui::GetStyle();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		//style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 0.f;
+		style.Colors[ImGuiCol_DockingEmptyBg].w = 0.f;
+	}
+    ImGui_ImplSDL2_InitForOpenGL(window, context);
+	ImGui_ImplOpenGL3_Init("#version 330");
+
+    bool is_running = true;
+    while (is_running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            switch (event.type) {
+                case SDL_QUIT:
+                    is_running = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplSDL2_NewFrame(window);
+		ImGui::NewFrame();
+
+        ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui::Begin("Test Window");
+		
+		ImGui::Text("Hello, world!");
+		ImGui::Button("I am Pressy the button!");
+		float values[5] = {0.5, 0.4, 0.3, 0.56, 0.46};
+		ImGui::PlotHistogram("I am a plot!", values, 5);
+		
+		ImGui::End();
+
+        ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
+
+    SDL_Quit();
 
     return 0;
 }
