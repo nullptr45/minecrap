dd66a589b784f31a8e057cdb42b2701f6eee215f
Nicolas Papirakis <whitercactus927@gmail.com>
2026-02-04 15:14:47 -0800

    Clear screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,65 @@
+#include <glad/glad.h>
+#include <SDL.h>
+
 #include <iostream>
+
+static constexpr int WINDOW_WIDTH         = 1280;
+static constexpr int WINDOW_HEIGHT        = 720;
+static constexpr const char* WINDOW_TITLE = "Minecrap";
 
 int main()
 {
-    std::cout << "Test" << std::endl;
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "Failed to init SDL2: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH,
+                                          WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
+
+    if (!window) {
+        std::cerr << "Failed to open window" << std::endl;
+        return -1;
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+
+    SDL_GLContext gl_ctx = SDL_GL_CreateContext(window);
+    if (!gl_ctx) {
+        std::cerr << "Failed to create OpenGL context" << std::endl;
+        return -1;
+    }
+
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+        std::cerr << "Failed to load OpenGL extensions" << std::endl;
+        return -1;
+    }
+
+    SDL_GL_MakeCurrent(window, gl_ctx);
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glClearColor(0.53, 0.81, 0.92, 1.0);
+
+    bool is_running = true;
+    while (is_running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    is_running = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        SDL_GL_SwapWindow(window);
+    }
 
     return 0;
 }
